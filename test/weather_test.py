eb3601ba9f4fcd7506e7e063be3c5d59d3222a81
CaptainJi <jiqing19861123@163.com>
2020-12-07 11:49:41 +0800

    测试自动构建6

--- test/weather_test.py
+++ test/weather_test.py
@@ -40,7 +40,8 @@ class Weather(TestCase):
         print(f'Expect city = {exp_city}, while actual city = {act_city}')
         self.assertEqual(exp_city, act_city, f'Expect city = {exp_city}, while actual city = {act_city}')
 
-
+if __name__ == '__main__':
+    pass
 
 
 
